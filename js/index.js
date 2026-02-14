aa4d4570a0aec4af4b084e98a91d4ded8a4180f2
Zeyad Hussein <zeyad02_hussein@hotmail.com>
2026-02-15 01:35:09 +0200

    first commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,53 @@
-console.log("Portfolio Loaded Successfully");
+console.log("Portfolio Loaded Successfully 🚀");
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", function () {
+    navLinks.forEach((l) => l.classList.remove("active"));
+    this.classList.add("active");
+  });
+});
+document.querySelectorAll("a[href^='#']").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+const scrollBtn = document.createElement("button");
+scrollBtn.innerHTML = "⬆";
+scrollBtn.classList.add("scroll-top-btn");
+document.body.appendChild(scrollBtn);
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 400) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+});
+
+scrollBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+const sections = document.querySelectorAll(".section");
+
+const revealOnScroll = () => {
+  sections.forEach((sec) => {
+    const sectionTop = sec.getBoundingClientRect().top;
+    const screenHeight = window.innerHeight;
+
+    if (sectionTop < screenHeight - 100) {
+      sec.classList.add("show-section");
+    }
+  });
+};
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
